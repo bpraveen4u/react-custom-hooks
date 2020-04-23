84b70817c84fe0781d116ed7355833ed29ebe662
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2020-04-23 10:06:12 +0530

    Custom hooks demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import DocTitleOne from './components/DocTitleOne'
+import DocTitleTwo from './components/DocTitleTwo'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import UserForm from './components/UserForm'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <UserForm />
+      {/* <CounterOne />
+      <CounterTwo /> */}
+      {/* <DocTitleOne />
+      <DocTitleTwo /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
